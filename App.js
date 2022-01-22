62f939141d54507c48306b8159306cda4a47d86d
paulinhorozendo <81476932+paulinhorozendo@users.noreply.github.com>
2022-01-22 14:23:15 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,58 @@
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
+import StyledButton, {
+  FancyButton,
+  SubmitButton,
+} from "./components/Button/Button";
+import { AnimatedLogo, DarkButton } from "./components/Button/Button.styles";
+
+const theme = {
+  dark: {
+    primary: "#000",
+    text: "#fff",
+  },
+  light: {
+    primary: "#fff",
+    text: "#000",
+  },
+  fontFamily: "Segoe UI",
+};
+
+const GlobalStyle = createGlobalStyle`
+button {
+  font-family: ${(props) => props.theme.fontFamily};
+}
+`;
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div className="App">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <AnimatedLogo src={logo} />
+        <StyledButton type="submit">Styled Button</StyledButton>
+        <div>
+          <br />
+        </div>
+        <StyledButton variant="outline">Styled Button</StyledButton>
+        <div>
+          <br />
+        </div>
+        <FancyButton as="a">Fancy Button</FancyButton>
+        <div>
+          <br />
+        </div>
+        <SubmitButton>Submit</SubmitButton>
+        <div>
+          <br />
+        </div>
+        <DarkButton>Dark Theme</DarkButton>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
